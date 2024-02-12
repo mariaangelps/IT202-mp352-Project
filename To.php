89a5de24d819e70efdac6287a578ada679074a23
Mariaapalacios <mangelpasa@gmail.com>
2024-02-11 23:21:38 -0500

    project_Errorss

--- To.php
+++ To.php
@@ -1,37 +1,36 @@
 
 <?php
-$error_message = '';
+
 $first_name = filter_input(INPUT_POST,'first_name');
 $last_name = filter_input(INPUT_POST,'last_name');
 $street_address = filter_input(INPUT_POST,'street_address');
 $city = filter_input(INPUT_POST,'city');
 $state = filter_input(INPUT_POST,'state');
 $zip_code = filter_input(INPUT_POST,'zip_code');
-$order_number = filter_input(INPUT_POST,'order_number');
-$shipping_date = filter_input(INPUT_POST,'shipping_date');
-$package_dimension = filter_input(INPUT_POST,'package_dimension');
-$total_value = filter_input(INPUT_POST,'total_value');
 
-
-//Check the value of the package is no more than 1000
-if($total_value<=1000){
-  $error_message.="The value cannot be more than 1000<br>";
-  else if($total_value<=0){
-    $error_message.= "Please enter a valid quantity<br>";
-  }
-} 
-
-//Check that dimensions are no more than 36 inches
-if($package_dimension>36){
-  $error_message.="The package size must not exceed 36 inches<br>";
-  else if($package_dimension<=0){
-    $error_message.="Please enter a valid size<br>";
-  }
+// Check if it exists, otherwise create it
+if(!isset($first_name)){
+  $first_name='';
 }
+if(!isset($last_name)){
+  $last_name='';
+}
+if(!isset($street_address)){
+  $street_address='';
+}
+if(!isset($city)){
+  $city='';
+}
+if(!isset($zip_code)){
+  $zip_code='';
+}
+
+//Validate Data
+$error_message = '';
 
 //check ZIP CODE
 if(!ctype_digit($zip_code)){
-  $error_message.="Please enter a valid ZIP CODE";
+  $error_message.="Please enter a valid ZIP CODE<br>";
 }
 
 //Check state 
@@ -65,19 +64,14 @@ if(!ctype_alpha($state)){
       <br> 
       <label> Zip Code:</label>
       <span<?php echo $zip_code;?></span>
-      <br>
-      <label> Order Number:</label>
-      <span><?php echp $order_number;?></span>
-      <br>
-      <label> Shipping Date: </label> 
-      <span><?php echo $shipping_date;?></span>
-      <br>
-      <label> Package Dimension: </label>
-      <span><?php echo $package_dimension;?></span>
-      <br>
-      <label> Total Value: </label>
-      <span><?php echo %$total_value;?></span>
-
+      
+      <?php
+     if(!empty($error_message)){
+      echo "<p>";
+      echo $error_message;
+      echo "<p>";
+     }
+      ?>
     </body>
 </html>
 
--- shipping_form.php
+++ shipping_form.php
@@ -1,72 +1,83 @@
 <?php
-if(!isset($first_name)){
-  $first_name='';
-}
-if(!isset($last_name)){
-  $last_name='';
-}
-if(!isset($street_address)){
-  $street_address='';
-}
-if(!isset($city)){
-  $city='';
-}
-if(!isset($state)){
-  $state='';
-}
-if(!isset($zip_code)){
-  $zip_code='';
-}
-if(!isset($order_number)){
-  $order_number='';
-}
-if(!isset($shipping_date)){
-  $shipping_date='';
-}
-if(!isset($package_dimension)){
-  $package_dimension='';
-}
-if(!isset($total_value)){
-  $total_value='';
-}
-?>
+ 
+
+$tracking_number = filter_input(INPUT_POST,'tracking_number');
+$shipping_date = filter_input(INPUT_POST,'shipping_date');
+$shipping_class= filter_input(INPUT_POST,'shipping_class');
+$shipping_company = filter_input(INPUT_POST,'shipping_company');
+$package_dimension = filter_input(INPUT_POST,'package_dimension',FILTER_VALIDATE_INT);
+$total_value = filter_input(INPUT_POST,'total_value',FILTER_VALIDATE_FLOAT);
+$from_address=filter_input(INPUT_POST,'from_address');
+$street_address = filter_input(INPUT_POST,'street_address');
+
+
+  //Validate Data 
+  $error_message = '';
+      
+  //Check the value of the package is no more than 1000
+  if($total_value===FALSE){
+    $error_message.= "Enter a valid number!";}
+    else if ($total_value>1000){
+      $error_message.= 'The value cannot be more than 1000<br>'; 
+    }
+  
+
+  //Check that dimensions are no more than 36 inches
+  if($package_dimension===FALSE){
+    $error_message.="Enter a valid number";
+  }
+    else if($package_dimension>36){
+      $error_message.='The package size must not exceed 36 inches<br>';}
+   
+       // Display the error message if it's not empty
+    if (!empty($error_message)) {
+      echo "<div style='color: red;'>$error_message</div>";
+  }
+  ?>
+
+
+
 <html> 
   <head> 
-    <title> SHIPPING ORDER: </title>
+    <title> GENERAL INFROMATION: </title>
   </head>
 
   <body> 
-    <?php
-  //check whether the value is empty. The empty syntax will always be
-  if(!empty($error_message)){
-      echo "<p>";
-      echo $error_message;
-      echo "</p>";
-            }
-            ?>
+    <h1> GENERAL INFROMATION: </h1>
+
+
     <form action = "to.php" method = "post">
-      <input type = "text" name= "first_name" value="<?php echo htmlspecialchars($first_name);?>"/>
+      <label> Store Address: </label>
+      <input type = "text" name = "store_address" value="<?php echo htmlspecialchars($from_address);?>"/>
       <br>
-      <input type= "text" name="last_name" value="<?php echo htmlspecialchars($first_name);?>"/>
+      <label> Client Address: </label>
+      <input type = "text" name = "client_address" value="<?php echo htmlspecialchars($street_address);?>"/>
       <br>
-      <input type = "text" name = "street_address" value ="<?php echo htmlspecialchars($first_name);?>"/>
+      <label> Shipping Date: </label> 
+      <input type = "text" name = "shipping_date " value="<?php echo htmlspecialchars($shipping_date);?>"/>
       <br>
-      <input type = "text" name= " $city ">
+      <label> Package Dimension: </label>
+      <input type = "text" name= "package_dimension" value="<?php echo htmlspecialchars($package_dimension);?>"/>
       <br>
-      <input type = "text" name= " $city "> <!-- termminar de poner las variables -->
+      <label> Package Declared Value: </label>
+      <input type = "text" name= "total_value" value="<?php echo htmlspecialchars($total_value);?>"/>
+      <br>
+      <label> Shipping Company: </label>
+      <input type= "text" name= "shipping_company" value= "<?php echo htmlspecialchars($shipping_company);?>"/>
+      <br>
+      <label> Shipping Class: </label>
+      <input type= "text" name= "shipping_class" value= "<?php echo htmlspecialchars($shipping_class);?>"/>
+      <br>
+      <label> Tracking Number: </label>
+      <input type = "text" name= "order_number" value="<?php echo htmlspecialchars($tracking_number);?>"/>
+  
+      <br>
+      <label> Barcode: </label> <!--insertar imagen-->
       <br>
 
-      <br>
-
-      <br>
-
-      <br>
-
-      <br>
-
-      <br>
-
-      <br>
+     
+    
+      <input type = "submit" value= "send"/>
     </form>
   </body>          
 </html>
