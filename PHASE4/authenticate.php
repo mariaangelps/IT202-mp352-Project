b31a320b5d901f9ee751af64736bc689211229b9
Mariaapalacios <mangelpasa@gmail.com>
2024-04-01 23:31:13 -0400

    now it definitely works validation

--- PHASE4/authenticate.php
+++ PHASE4/authenticate.php
@@ -1,30 +1,27 @@
-<?php 
+<?php
+
 require_once('managers.php');
-require_once('database_njit.php');
 session_start();
 
 $emailAddress = filter_input(INPUT_POST, 'emailAddress');
 $password = filter_input(INPUT_POST, 'password');
-$firstName = filter_input(INPUT_POST, 'firstName');
-$lastName = filter_input(INPUT_POST, 'lastName');
-
-echo "<p>$emailAddress</p>";
 
 if (is_valid_admin_login($emailAddress, $password)) {
-  echo "si funciona";
-  
     $manager_details = managers_credentials($emailAddress);
-    $_SESSION['valid_credentials'] = true;
-    $_SESSION['managers_credentials'] = $manager_details;
-    echo "Welcome, " . managers_credentials($emailAddress)['firstName'] . "!<br>";
-    echo "<p>You have successfully logged in.</p>";
-} else {
-    if ($emailAddress == NULL && $password == NULL) {
-        $login_message = 'You must login to view this page.';
+    if ($manager_details) {
+        $_SESSION['is_valid_admin'] = true;
+        // Output welcome message using manager details
+        echo "Welcome, " . $manager_details['firstName'] . "!<br>";
+        echo "<p>You have successfully logged in.</p>";
     } else {
-        $login_message = 'Invalid credentials.';
+        //credentials not found
+        echo "Credentials not found!";
+        
     }
-    
+} else {
+    // Handle invalid login attempt
+    echo "Invalid credentials.";
     include('login.php');
 }
+
 ?>
--- PHASE4/managers.php
+++ PHASE4/managers.php
@@ -2,29 +2,7 @@
 
 require_once('database_njit.php');
 
-/*function addsportsequipmentmanager($emailAddress, $password, $firstName, $lastName, $dateCreated) {
-    $db = getDB();
-    $hash = password_hash($password, PASSWORD_DEFAULT);
-    $query = 'INSERT INTO sportsequipmentManagers (emailAddress, password, firstName, lastName, dateCreated)
-             VALUES (:emailAddress, :password, :firstName, :lastName, :dateCreated)';
-    $statement = $db->prepare($query);
-    $statement->bindValue(':emailAddress', $emailAddress);
-    $statement->bindValue(':password', $hash);
-    $statement->bindValue(':firstName', $firstName);
-    $statement->bindValue(':lastName', $lastName);
-    $statement->bindValue(':dateCreated', $dateCreated);
-    $success = $statement->execute(); 
-}
-
-
-// Adding sportsequipmentmanagers
-addsportsequipmentmanager('marita@gmail.com', 'mari123', 'Marita', 'Brown', date('Y-m-d H:i:s'));
-addsportsequipmentmanager('juandp43@outlook.com', 'mynameisJuan', 'Juan Diego', 'Zaleski', date('Y-m-d H:i:s'));
-addsportsequipmentmanager('crisbs48@gmail.com', 'holasoyCristina345', 'Cristina', 'Solis', date('Y-m-d H:i:s'));
-*/
-
 function is_valid_admin_login($emailAddress, $password) {
-    //echo "Validating Admin Login...<br>";
     $db = getDB();
     $query = 'SELECT password FROM sportsequipmentManagers WHERE emailAddress = :emailAddress';
     $statement = $db->prepare($query);
@@ -34,22 +12,18 @@ function is_valid_admin_login($emailAddress, $password) {
     $statement->closeCursor(); 
     
     if ($row == false) {
-        echo "User not found!<br>";
         return false;
     } else {
         $hash = $row['password'];
         if (password_verify($password, $hash)) {
-            echo "Login successful!<br>";
             return true;
         } else {
-            echo "Incorrect password!<br>";
             return false;
         }
     }
 }
 
 function managers_credentials($emailAddress) {
-    echo "Working Manager Credentials...<br>";
     $db = getDB();
     $query = 'SELECT emailAddress, firstName, lastName FROM sportsequipmentManagers WHERE emailAddress = :emailAddress';
     $statement = $db->prepare($query);
@@ -59,14 +33,10 @@ function managers_credentials($emailAddress) {
     $statement->closeCursor();
     
     if ($result) {
-        echo "Manager credentials found!<br>";
         return $result;
     } else {
-        echo "Manager not found!<br>";
         return false;
     }
 }
-
-
 
 ?>
