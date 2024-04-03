eb1ec94ac256cd102666a4d398538f2aba7ba0a1
Mariaapalacios <mangelpasa@gmail.com>
2024-04-03 13:36:05 -0400

    final final check, names and date added
    maaria's names added at the top of each file

--- PHASE4/authenticate.php
+++ PHASE4/authenticate.php
@@ -18,10 +18,11 @@ if (is_valid_admin_login($emailAddress, $password)) {
         $_SESSION['emailAddress'] = $emailAddress;
         $_SESSION['firstName'] = $firstName;
         $_SESSION['lastName'] = $lastName;
-        include('home.php');
-        // Output welcome message using manager details
+        include('home.php'); // automatically the welcome message displayed 
+        /* Output welcome message using manager details
         echo "<br>";
         echo "<h3><br>     Welcome, " . $manager_details['firstName'] . " " . $manager_details['lastName'] ."<br>" . $manager_details['emailAddress'] . "!<br> </h3>";
+        */
         echo "<h4>      You have successfully logged in.</h3>";
         
     } else {
